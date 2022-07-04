f3b05b104d5a67c485e41da90d37cf229c3fab94
gohyunseo <108450061+gohyunseo@users.noreply.github.com>
2022-07-04 01:32:15 -0700

    day3 study

--- day3_4.java
+++ day3_4.java
@@ -3,10 +3,10 @@ public class day3_4 {
     public static void main(String[] args) {
     
     for(int i = 1; i<= 5; i++){
-    for(int j = 1; j<=10; j++){
-    System.out.print("*");}
-    System.out.println();
-    
+        for(int j = 1; j<=10; j++){
+             System.out.print("*");
+            }
+            System.out.println();
     }
-}
+  }
 }
